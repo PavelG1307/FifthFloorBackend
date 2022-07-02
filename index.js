ac5d0ea222d938b30f63cc4d01f5327031007bd8
PavelG1307 <julu13@yandex.ru>
2022-07-02 10:26:57 +0300

    developing modules

--- index.js
+++ index.js
@@ -47,6 +47,7 @@ async function answer(ws, message) {
             case "ADD STATION":
                 return await deviceControllers.addStation(user.id, message.key)
             case "SET BRIGHTNESS":
+                console.log(message)
                 return await deviceControllers.setBrightness(user.id, message.brightness)
             default:
                 return {error: "Bad request"}
