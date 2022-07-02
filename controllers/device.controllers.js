f8ccdf21f97ff010666fc3f054d2140d3fa65cac
PavelG1307 <julu13@yandex.ru>
2022-07-02 03:36:45 +0300

    developing modules

--- controllers/device.controllers.js
+++ controllers/device.controllers.js
@@ -226,7 +226,7 @@ class DeviceControllers{
     }
 
     async getUserIdFromStationId(id){
-        return (await db.query(`SELECT user_id FROM stations WHERE id = $1`,[id])).rows
+        return (await db.query(`SELECT user_id FROM stations WHERE id = $1`,[id])).rows[0].user_id
     }
 }
 const deviceControllers = new DeviceControllers()
